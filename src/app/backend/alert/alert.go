24212098a0e50dc16aa9f6c21ff52af04833bf2d
Zhiyong ZY46 Li <lizy46@Lenovo.com>
2018-12-14 14:12:23 +0800

    bug fix

--- src/app/backend/alert/alert.go
+++ src/app/backend/alert/alert.go
@@ -104,9 +104,11 @@ func (s *AlertStore) alertsHandler(w http.ResponseWriter, r *http.Request) {
 // alerts get
 func (s *AlertStore) getHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+	itemsPerPage, _ := strconv.Atoi(r.Form["itemsPerPage"]
+	page, _ := strconv.Atoi(r.Form["page"])
 	var p  = AlertPageIndex{
-		itemsPerPage : strconv.Atoi(r.Form["itemsPerPage"]),
-		page : strconv.Atoi(r.Form["page"]),
+		itemsPerPage : itemsPerPage,
+		page : page,
 	}
 	/*dec := json.NewDecoder(r.Body)
 	defer r.Body.Close()
