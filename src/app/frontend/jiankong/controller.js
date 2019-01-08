b16d266a34cc715884333437f79ecdf2e50db8d3
Gaoyuan <gaoyuan20@lenovo.com>
2019-01-08 16:04:20 +0800

    日志中心监控中心地址

--- src/app/frontend/jiankong/controller.js
+++ src/app/frontend/jiankong/controller.js
@@ -3,4 +3,12 @@ export class jiankongController {
      * @ngInject
      */
     constructor() {}
+
+    /**
+     * 监控中心地址
+     * @export
+     */
+    url() {
+        return `http://${location.hostname}:30000`
+    }
 }
--- src/app/frontend/rizhi/controller.js
+++ src/app/frontend/rizhi/controller.js
@@ -3,4 +3,12 @@ export class rizhiController {
      * @ngInject
      */
     constructor() {}
+
+    /**
+     * 日志中心地址
+     * @export
+     */
+    url() {
+        return `http://${location.hostname}:30001`
+    }
 }
