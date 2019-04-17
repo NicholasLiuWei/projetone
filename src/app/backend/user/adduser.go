a849a45b4ca07984026de8672d43098a7479af65
ZhiBin ZB8 Wang <wangzb8@lenovo.com>
2019-04-17 14:19:36 +0800

    add log for debug MU.

--- src/app/backend/user/adduser.go
+++ src/app/backend/user/adduser.go
@@ -15,31 +15,31 @@
 package user
 
 import (
-	"k8s.io/client-go/kubernetes"
+	namespace2 "github.com/kubernetes/dashboard/src/app/backend/resource/namespace"
+	"github.com/kubernetes/dashboard/src/app/backend/settings/api"
 	"k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"github.com/kubernetes/dashboard/src/app/backend/settings/api"
+	"k8s.io/client-go/kubernetes"
 	"log"
-	namespace2 "github.com/kubernetes/dashboard/src/app/backend/resource/namespace"
 )
 
-
-func HandleCreatUser(client kubernetes.Interface,user *UserSpec) ErrResponse{
-	userName:=UserConfigMapPrefix+user.Username
-    //if exists
-	ConfigMap, err := client.CoreV1().ConfigMaps(api.SettingsConfigMapNamespace).Get(userName,metaV1.GetOptions{})
-	if err == nil && ConfigMap!=nil {
+func HandleCreatUser(client kubernetes.Interface, user *UserSpec) ErrResponse {
+	userName := UserConfigMapPrefix + user.Username
+	//if exists
+	ConfigMap, err := client.CoreV1().ConfigMaps(api.SettingsConfigMapNamespace).Get(userName, metaV1.GetOptions{})
+	if err == nil && ConfigMap != nil {
 		log.Println(err)
 		return ErrUserAlreadyExist
 	}
-    NameSpaceSpec:=&namespace2.NamespaceSpec{
-	Name:user.Username,
-    }
-    err = namespace2.CreateNamespace(NameSpaceSpec,client)
-    if err!=nil{
-	   return ErrResponse{51000,err.Error()}
-    }
-	UserConfig:=&v1.ConfigMap{
+	NameSpaceSpec := &namespace2.NamespaceSpec{
+		Name: user.Username,
+	}
+	err = namespace2.CreateNamespace(NameSpaceSpec, client)
+	log.Println("CreateNamespace", NameSpaceSpec)
+	if err != nil {
+		return ErrResponse{51000, err.Error()}
+	}
+	UserConfig := &v1.ConfigMap{
 		TypeMeta: metaV1.TypeMeta{
 			Kind:       api.ConfigMapKindName,
 			APIVersion: api.ConfigMapAPIVersion,
@@ -47,20 +47,21 @@ func HandleCreatUser(client kubernetes.Interface,user *UserSpec) ErrResponse{
 		ObjectMeta: metaV1.ObjectMeta{
 			Name:      userName,
 			Namespace: api.SettingsConfigMapNamespace,
-			Labels:map[string]string{"role":UserConfigMapRole},
+			Labels:    map[string]string{"role": UserConfigMapRole},
 		},
 		Data: map[string]string{
-			"username":user.Username,
-			"password":user.Password,
-			"email":user.Email,
-			"isAdmin":FormatBool(user.IsAdmin)},
-		}
-	_, err =client.CoreV1().ConfigMaps(api.SettingsConfigMapNamespace).Create(UserConfig)
-	if err!=nil{
-         return ErrResponse{51000,err.Error()}
-	 }
-	 return ErrResponse{0,"ok"}
+			"username": user.Username,
+			"password": user.Password,
+			"email":    user.Email,
+			"isAdmin":  FormatBool(user.IsAdmin)},
 	}
+	_, err = client.CoreV1().ConfigMaps(api.SettingsConfigMapNamespace).Create(UserConfig)
+	log.Println("CreateUser", userName)
+	if err != nil {
+		return ErrResponse{51000, err.Error()}
+	}
+	return ErrResponse{0, "ok"}
+}
 
 func FormatBool(b bool) string {
 	if b {
