8f0ac93c1de8d5bffda1b76f96e732d32448ba5c
Gaoyuan <gaoyuan20@lenovo.com>
2019-04-28 15:22:12 +0800

    bug fix

--- src/app/frontend/rizhi/pvc/controller.js
+++ src/app/frontend/rizhi/pvc/controller.js
@@ -2,7 +2,12 @@ export class pvcController {
     /**
      * @ngInject
      */
-    constructor($resource, toastr) {
+    constructor($resource, toastr, $mdDateLocaleProvider) {
+        $mdDateLocaleProvider.months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+        $mdDateLocaleProvider.shortMonths = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+        // $mdDateLocaleProvider.days = ['dimanche', 'lundi', 'mardi', ...];
+
+        $mdDateLocaleProvider.shortDays = ['日', '一', '二', '三', '四', '五', '六'];
         /** @export */
         this.resource = $resource;
 
